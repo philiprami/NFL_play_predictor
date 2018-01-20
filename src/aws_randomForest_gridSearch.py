928a9b8dd48daa54c37ff961b98094441c108779
Philip Ramirez <Philip@Philips-MacBook-Air.local>
2018-01-20 13:42:10 -0800

    scrape test data, data exploration

--- src/aws_randomForest_gridSearch.py
+++ src/aws_randomForest_gridSearch.py
@@ -7,11 +7,15 @@ df = pd.read_csv('../data/master_RushPassOnly.csv')
 y = df.pop('IsPass').values
 X = df.values
 
-param_grid = {"max_depth": [5, 10],
-              "max_features": [None, 'auto'],
-              "n_estimators": [1000, 5000]}
+param_grid = {"max_depth": [3, 5, 10, None],
+              "max_features": [None, 'auto', 'log2'],
+              "n_estimators": [100],
+              'min_samples_split': [2, 4],
+              'min_samples_leaf': [1, 2, 4],
+              'bootstrap': [True, False]}
 
-gs = grid_search.GridSearchCV(RandomForestClassifier(verbose=2, n_jobs=-1), param_grid=param_grid, n_jobs=-1, verbose=2)
+rf = RandomForestClassifier(verbose=2, n_jobs=-1)
+gs = GridSearchCV(rf, param_grid=param_grid, n_jobs=-1, verbose=2, scoring='neg_mean_squared_error')
 gs.fit(X, y)
 best_parameters = gs.best_params_
 print best_parameters
